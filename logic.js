b8b3aafc6ef6092218d0121b5a1d7914eb97045f
esposito.00@outlook.it <esposito.00@outlook.it>
2022-11-04 16:00:56 +0100

    changes

--- logic.js
+++ logic.js
@@ -1,3 +1,6 @@
+// PASSWORD GENERATOR
+
+
 // chiedere all'utente il suo nome
 let nome = prompt("Inserisci il tuo nome");
 console.log(nome);
@@ -11,4 +14,4 @@ let colore = prompt("Inserisci il tuo colore preferito");
 console.log(colore);
 
 // output
-document.getElementById("first-title").innerHTML = `Ti chiami ${nome} ${cognome} e il ${colore} è il tuo colore preferito;`
+document.getElementById("first-title").innerHTML = `${nome}${cognome}${colore}21`;
