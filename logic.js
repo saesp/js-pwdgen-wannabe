c520232df361c9973d73a488a2228d8d77c438cd
esposito.00@outlook.it <esposito.00@outlook.it>
2022-11-04 15:44:19 +0100

    Completed

---
+++ logic.js
@@ -0,0 +1,14 @@
+// chiedere all'utente il suo nome
+let nome = prompt("Inserisci il tuo nome");
+console.log(nome);
+
+// chiedere il suo cognome
+let cognome = prompt("Inserisci il tuo cognome");
+console.log(cognome);
+
+// chiedere il suo colore preferito
+let colore = prompt("Inserisci il tuo colore preferito");
+console.log(colore);
+
+// output
+document.getElementById("first-title").innerHTML = `Ti chiami ${nome} ${cognome} e il ${colore} è il tuo colore preferito; 
